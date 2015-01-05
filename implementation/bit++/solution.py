a286239b0d2c20630c6e1ee64c9f005ddb2cfec0
Nikola Shahpazov <nshahpazov@gmail.com>
2015-01-05 12:51:07 +0200

    optimized even more

--- implementation/bit++/solution.py
+++ implementation/bit++/solution.py
@@ -1,5 +1,2 @@
-sum = 0
-for _ in range(input()):
-    sum += [-1, 1]["+" in raw_input()]
-print sum
-# print sum(44 - ord(raw_input()[1])for _ in range(input()))
+print sum([-1, 1]["+" in raw_input()] for _ in range(input()))
+#print sum(44 - ord(raw_input()[1])for _ in range(input()))
