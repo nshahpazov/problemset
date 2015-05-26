61fa2abb565b5e3bb447981b492297a25529f52a
nshahpazov <nshahpazov@gmail.com>
2015-05-26 17:48:14 +0300

    modularized better

--- matr/solution.js
+++ matr/solution.js
@@ -1,10 +1,7 @@
-/*********************************************
- *** Solution to flowing a character table ***
-**********************************************/
-
-(function () {
+module.exports = (function () {
   'use strict';
 
+  // transpose a matrix
   function transpose(matrix) {
     return matrix[0].map(function (col, i) {
       return matrix.map(function (row) {
@@ -13,29 +10,20 @@
     });
   };
 
-  function flowList(col) {
-    return col.sort(function (a, b) {
+  // flow left-way empty strings
+  function flowList(list) {
+    // maybe use a priority queue?
+    return list.sort(function (a, b) {
       return b === ' ' ? 1 : 0;
     });
   };
 
-  function flowMatrix(matr) {
-    var transposedResult = transpose(matr).map(function (col) {
-      return flowList(col);
+  // export module function
+  return function (matrix) {
+    var transposedResult = transpose(matrix).map(function (column) {
+      return flowList(column);
     });
     // transpose back to normal
     return transpose(transposedResult);
   };
-
-  // test matrix
-  var matrix = [
-    ['z', 'z', 'c', ' ', '1', 'z', 'x', 'd'],
-    ['y', 'y', ' ', 'z', 'd', 'a', 'x', 'x'],
-    [' ', ' ', 'z', ' ', ' ', 'y', 'x', ' '],
-    ['a', 'a', ' ', 'd', 'd', 'u', 'x', 'a'],
-    ['d', ' ', 'a', ' ', ' ', 't', 'x', ' ']
-  ];
-
-  var result = flowMatrix(matrix);
-  console.log(result);
 })();
