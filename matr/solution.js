48f59862f1eb9d93ccbb86801f72a79dd690dd26
nshahpazov <nshahpazov@gmail.com>
2015-05-26 16:57:54 +0300

    encapsulated

--- matr/solution.js
+++ matr/solution.js
@@ -1,33 +1,41 @@
-function transpose(matrix) {
-  return matrix[0].map(function (col, i) {
-    return matrix.map(function (row) {
-      return row[i];
+/*********************************************
+ *** Solution to flowing a character table ***
+**********************************************/
+
+(function () {
+  'use strict';
+
+  function transpose(matrix) {
+    return matrix[0].map(function (col, i) {
+      return matrix.map(function (row) {
+        return row[i];
+      });
     });
-  });
-};
+  };
 
-function flowList(col) {
-  return col.sort(function (a, b) {
-    return b === ' ' ? 1 : 0;
-  });
-};
+  function flowList(col) {
+    return col.sort(function (a, b) {
+      return b === ' ' ? 1 : 0;
+    });
+  };
 
-function flowMatrix(matr) {
-  var transposedResult = transpose(matr).map(function (col) {
-    return flowList(col);
-  });
-  // transpose back to normal
-  return transpose(transposedResult);
-};
+  function flowMatrix(matr) {
+    var transposedResult = transpose(matr).map(function (col) {
+      return flowList(col);
+    });
+    // transpose back to normal
+    return transpose(transposedResult);
+  };
 
-// test matrix
-var matrix = [
-  ['z', 'z', 'c', ' ', '1', 'z', 'x', 'd'],
-  ['y', 'y', ' ', 'z', 'd', 'a', 'x', 'x'],
-  [' ', ' ', 'z', ' ', ' ', 'y', 'x', ' '],
-  ['a', 'a', ' ', 'd', 'd', 'u', 'x', 'a'],
-  ['d', ' ', 'a', ' ', ' ', 't', 'x', ' ']
-];
+  // test matrix
+  var matrix = [
+    ['z', 'z', 'c', ' ', '1', 'z', 'x', 'd'],
+    ['y', 'y', ' ', 'z', 'd', 'a', 'x', 'x'],
+    [' ', ' ', 'z', ' ', ' ', 'y', 'x', ' '],
+    ['a', 'a', ' ', 'd', 'd', 'u', 'x', 'a'],
+    ['d', ' ', 'a', ' ', ' ', 't', 'x', ' ']
+  ];
 
-var result = flowMatrix(matrix);
-console.log(result);
+  var result = flowMatrix(matrix);
+  console.log(result);
+})();
